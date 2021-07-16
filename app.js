cf8d523c435d42fc37c0e366f77586438fc8cc82
sharad <manuandmehrotra@gmail.com>
2021-07-16 18:59:13 +0530

    pvt R

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+//Import Routes
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+dotenv.config();
+
+//connect to DB
+mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser : true}, ()=>
+console.log('Connected to DB')
+);
+
+//middlewares
+app.use(express.json());
+
+app.use('/api/user',authRoute);
+app.use('/api/posts',postRoute);
+  
+app.listen(3000,() => console.log('Server up and running'));
---
+++ routes/posts.js
@@ -0,0 +1,14 @@
+// just to test jwt, making routes private
+const router = require('express').Router();
+const verify = require('../verifyToken');
+
+router.get('/',verify,(req,res) => {
+    res.json({
+        posts:{
+            title:'my first post',
+            description :'random data u shouldnt access'
+        }
+    });
+});
+
+module.exports =  router;
